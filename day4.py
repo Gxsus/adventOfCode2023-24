94b1dba009da380fea9dc83564739fc22eefb0d9
Simone Gesualdi <113978761+Gxsus@users.noreply.github.com>
2023-12-13 22:15:31 +0100

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,48 @@
+#Part one
+ris = 0
+with open('input.txt', 'r') as file:
+    for i in range(1, 221):
+        numbers = file.readline().strip().replace("Card {:3d}: ".format(i), '').split(' | ')
+        winning_numbers = []
+        for n in numbers[0].split(' '):
+            if n != '':
+                winning_numbers.append(n)
+        got_numbers = 0
+        for n in numbers[1].split(' '):
+            if n in winning_numbers:
+                got_numbers += 1
+        if got_numbers > 0:
+            ris += (2 ** (got_numbers - 1))
+
+print(ris)
+
+#Part two
+ris = 220
+card_wins = []
+
+
+def sum_tickets(ris, card_wins, t):
+    ris += card_wins[t]
+    for i in range(t, t + card_wins[t]):
+        sum_tickets(ris, card_wins, t + i)
+
+with open('input.txt', 'r') as file:
+    for i in range(1, 221):
+        numbers = file.readline().strip().replace("Card {:3d}: ".format(i), '').split(' | ')
+        winning_numbers = []
+        for n in numbers[0].split(' '):
+            if n != '':
+                winning_numbers.append(n)
+        got_numbers = 0
+        for n in numbers[1].split(' '):
+            if n in winning_numbers:
+                got_numbers += 1
+        card_wins.append(got_numbers)
+
+got_card = {_ : 1 for _ in range(220)}
+for i in range(220):
+    ris += card_wins[i] * got_card[i]
+    for j in range(i + 1, i + card_wins[i] + 1):
+        got_card[j] += got_card[i]
+
+print(ris)
